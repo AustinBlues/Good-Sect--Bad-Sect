75647a8121d61de43de1f7fed70cc36712be53d1
Philip Dhingra <philipkd@gmail.com>
2012-02-25 15:56:34 -0600

    new buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,3 +3,17 @@
 
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+
+function show_raw_data() {
+	$('#raw_data').toggle();
+}
+
+$(document).ready(function($) {
+	$('a#show_raw_data_btn').click(toggle);
+});
+
