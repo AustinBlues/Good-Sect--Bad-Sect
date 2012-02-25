accda021d9c2ad4aa2a1252c5b9d078eb5f9ce40
Philip Dhingra <philipkd@gmail.com>
2012-02-25 14:15:08 -0600

    has jquery stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Good Sect Bad Sect"
     if @title.nil?
       base_title
     else
@@ -11,6 +11,7 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    ""
+    #image_tag("logo.png", :alt => "Sample App", :class => "round")"
   end
 end
