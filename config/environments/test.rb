7562f8a14e60b15cd57cc75e6acfb523e306aa59
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2012-03-21 11:51:48 -0500

    Rails 3.0 => 3.1 migration.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Disable assets pipeline
+  config.assets.enabled = false
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
 end
