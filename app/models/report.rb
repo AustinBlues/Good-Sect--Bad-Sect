a0d3431ae2861b86d0e5073b057d0772df374d82
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2012-03-21 16:18:07 -0500

    REFACTOR: move date w/ fallbacks into model.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,6 @@
 class Report < ActiveRecord::Base
+  # Date as string in current timezone, with fallbacks.
+  def date
+    (datetime || updated_at || created_at).localtime.strftime('%b %d, %Y')
+  end
 end
