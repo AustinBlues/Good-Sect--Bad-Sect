acf5d8c3333ffef153bcd70997c76af8218d993e
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2012-04-03 15:40:07 -0500

    Add optional user to reports model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327195308) do
+ActiveRecord::Schema.define(:version => 20120403191525) do
 
   create_table "reports", :force => true do |t|
     t.string   "lat"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120327195308) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "good",       :default => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
